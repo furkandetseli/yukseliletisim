4d7e1a1f692ea1456e04637d5d182ff7d1670a27
Furkan Detseli <68570706+furkandetseli@users.noreply.github.com>
2024-12-24 05:57:05 -0800

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -8,7 +8,6 @@ import string
 from sqlalchemy.orm import validates
 from slugify import slugify  # Başa ekleyin
 
-
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
